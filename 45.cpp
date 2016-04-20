d93bcaad182d7d26f4329ad1f8ae0c85e0d149a0
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-04-20 08:22:10 -0700

    blahblah

---
+++ 45.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+    int id{3};
+    string name{"Mike"};
+public:
+    Test() = default;
+    Test( const Test &other) = delete;
+    Test &operator=(const Test &other) = delete;
+    Test(int id) : id{id} {}
+    void print() {
+        cout << id << " : " << name << endl;
+    }
+};
+
+int main() {
+
+    Test test;
+    test.print();
+
+    Test test1(77);
+    test1.print();
+
+    // deleted copy constructor
+    //Test test2 = test1;
+
+    return 0;
+}
